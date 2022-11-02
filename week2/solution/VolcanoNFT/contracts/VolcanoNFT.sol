c9b720ad82c36a2da573daf0efd0b189772a6a02
0xsenzel <0xsenzel@gmail.com>
2022-11-02 18:07:22 +0800

    Update VolcanoNFT

--- week2/solution/VolcanoNFT/contracts/VolcanoNFT.sol
+++ week2/solution/VolcanoNFT/contracts/VolcanoNFT.sol
@@ -2,11 +2,11 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract VolcanoNFT is ERC721("Volcano", "VOL") {
+contract VolcanoNFT is ERC721("Volcano", "VOL"), Ownable {
     uint256 tokenId;
     constructor() {
-
     }
 
     function mint() public {
